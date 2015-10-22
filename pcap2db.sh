f7c1ef0fea80e55d1480e43c1eba58cb46b9e430
SimonSK <SimonSK@users.noreply.github.com>
2015-10-22 02:11:34 -0500

    Update pcap2db.sh
    fail check on each script and exit with error code
    remove json file at the end

--- pcap2db.sh
+++ pcap2db.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# check argument
 if [ -z "$1" ]
     then
     echo "yo i need the filename"
     exit 1
 fi
 
+# pcap to json
 ./sniffyDB $1
+if [ $? -ne 0 ]; then
+    echo "pcap to json parse failed for some reason :("
+    exit 1
+fi
 
 pcap="pcap.json"
 
+# json to mysql database
 ./json2db.py $pcap
+if [ $? -ne 0 ]; then
+    echo "json to database parse failed for some reason :("
+fi
+
+# remove json file
+rm $pcap
+exit 0
