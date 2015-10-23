de8dd1beb3c82539f5d6a8e29bb80bf7fb391cee
Tentacle Sniffer <project@sniffy.co>
2015-10-22 23:37:07 -0400

    absolutes for pcap2db

--- pcap2db.sh
+++ pcap2db.sh
@@ -8,7 +8,7 @@ fi
 
 # pcap to json
 # sniffy alias for sniffyDB
-~/sniffydb/SniffyDB/sniffyDB $1
+/home/project/sniffydb/SniffyDB/sniffyDB $1
 if [ $? -ne 0 ]; then
     echo "pcap to json parse failed for some reason :("
     exit 1
@@ -18,7 +18,7 @@ pcap="pcap.json"
 
 # json to mysql database
 # j2d alias for json2db.py
-~/sniffydb/SniffyDB/json2db.py $pcap
+/home/project/sniffydb/SniffyDB/json2db.py $pcap
 if [ $? -ne 0 ]; then
     echo "json to database parse failed for some reason :("
 fi
