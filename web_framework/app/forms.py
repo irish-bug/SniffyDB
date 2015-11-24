5489fabd8eab24702144bc6b5641f08e241774bb
Donald Cha <ddcha3@illinois.edu>
2015-11-24 12:04:37 -0600

    change made for new file

--- web_framework/app/forms.py
+++ web_framework/app/forms.py
@@ -4,34 +4,23 @@ from wtforms.validators import DataRequired
 
 __author__ = "Donald Cha"
 
-class SubmitForm(Form):
+class PacketForm(Form):
 	src = StringField('Source IP', validators=[DataRequired()])	
 	dst = StringField('Destination IP', validators=[DataRequired()])
-	proto = StringField('Protocol', validators=[DataRequired()])
-	seqwindow = IntegerField('Sequence Window', validators=[DataRequired()])
+	protocol = IntegerField('Protocol', validators=[DataRequired()])
 	length = IntegerField('Packet Length', validators=[DataRequired()])
 	payload = StringField('Payload', validators=[DataRequired()])
-	time = StringField('Packet Time', validators=[DataRequired()])
-	PcapID = StringField('PcapID', validators=[DataRequired()])
-	PIN = IntegerField('PIN', validators=[DataRequired()])
+	packettime = StringField('Packet Time', validators=[DataRequired()])
+	pcapid = StringField('PcapID', validators=[DataRequired()])
+	pin = IntegerField('PIN', validators=[DataRequired()])
 	submit = SubmitField("Send")
 
-class DeleteForm(Form):
-	PcapID = StringField('PcapID', validators=[DataRequired()])
-	PIN = IntegerField('PIN', validators=[DataRequired()])
+class TagForm(Form):
+	tag = StringField('Tag', validators=[DataRequired()])
+	type_val = SelectField('Type', choices=[('DST', 'Destination IP'), ('SRC', 'Source IP')])
 	submit = SubmitField("Send")
 
-class SearchForm(Form):
-	PcapID = StringField('PcapID', validators=[DataRequired()])
-	PIN = IntegerField('PIN', validators=[DataRequired()])
-	submit = SubmitField("Send")
-
-class EditForm(Form):
-	PcapID = StringField('PcapID', validators=[DataRequired()])
-	PIN = IntegerField('PIN', validators=[DataRequired()])
-	select = SelectField('Field to Change', choices=[('src', 'Source IP'), ('dst', 'Destination IP'),
-							('protocol', 'Protocol'), ('seqwindow', 'Sequence Window'),
-							('len', 'Packet Length'), ('payload', 'Payload'),
-							('packettime', 'Packet Time')])
-	new_val = TextField('New Value', validators=[DataRequired()])
+class PacketCaptureForm(Form):
+	pcapid = StringField('PcapID', validators=[DataRequired()])
+	pcaptime = StringField('Pcap Generation Time', validators=[DataRequired()])
 	submit = SubmitField("Send")
--- web_framework/create_db.py
+++ web_framework/create_db.py
@@ -5,9 +5,9 @@ __author__ = "Donald Cha"
 class Database:
 
 	host = 'localhost'
-	user = 'root'
-	password = ''
-	db = 'testdb'
+	user = 'sniffydb_dev'
+	password = 'sniffy+DB'
+	db = 'sniffydb_main'
 
 	def __init__(self):
 		self.connection = MySQLdb.connect(self.host, self.user, 		self.password, self.db)
@@ -35,26 +35,50 @@ class Database:
 if __name__ == "__main__":
 
 	db = Database()
+	
+	# drop table if it already exists.
+	query = """
+	DROP TABLE IF EXISTS Packet
+	"""
+	db.query(query)
 
-	#CleanUp Operation
-	#del_query = "DELETE FROM basic_python_database"
-	#db.insert(del_query)
+	query = """
+	DROP TABLE IF EXISTS PacketCapture
+	"""
+	db.query(query)
+
+	query = """
+	DROP TABLE IF EXISTS Tag
+	"""
+	db.query(query)
+
+	query = """
+	DROP TABLE IF EXISTS Tagged
+	"""
+	db.query(query)
 
 	# Create table
 	query = """
-	CREATE TABLE pcap (dst VARCHAR(15), src VARCHAR(15), proto VARCHAR(100) DEFAULT 'UNKNOWN', seqwindow INT(100), length INT(100), payload VARCHAR(2000) DEFAULT 'NONE', time VARCHAR(100), PcapID VARCHAR(100), PIN INT(100), PRIMARY KEY (PcapID, PIN));
+	CREATE TABLE IF NOT EXISTS Packet (pcapid VARCHAR(255), pin INT, packettime VARCHAR(255) NOT NULL, src VARCHAR(15) DEFAULT NULL, dst VARCHAR(15) DEFAULT NULL, protocol INT DEFAULT -1, len INT DEFAULT 0, payload VARCHAR(2000) DEFAULT NULL, PRIMARY KEY (pcapid, pin));
 	"""
 
 	db.query(query)
-	#db.insert(query)
 
-	# Data retrieved from the table
-	#select_query = """
-	#SELECT * FROM basic_python_database
-	#WHERE age = 21
-	#"""
+	query = """
+	CREATE TABLE IF NOT EXISTS PacketCapture (pcapid VARCHAR(255), pcaptime VARCHAR(255) NOT NULL, PRIMARY KEY (pcapid));
+	"""
 
-	#people = db.query(select_query)
+	db.query(query)
 
-	#for person in people:
-	#print "Found %s " % person['name']
+	query = """
+	CREATE TABLE IF NOT EXISTS Tag (tagid INT NOT NULL AUTO_INCREMENT, tag VARCHAR(255) NOT NULL, type VARCHAR(3) DEFAULT "SRC", PRIMARY KEY (tagid));
+	"""
+
+	db.query(query)
+
+	query = """
+	CREATE TABLE IF NOT EXISTS Tagged (pcapid VARCHAR(255), pin INT, tagid INT, PRIMARY KEY (pcapid, pin, tagid));
+	"""
+
+	db.query(query)
+
