0fc4e9d4940c4b81c8fc2d1964629ae5eaa21808
SimonSK <simonsk@outlook.com>
2015-11-24 21:07:00 -0600

    script for adding dummy data

--- dummy_data.py
+++ dummy_data.py
@@ -1,22 +1,48 @@
 #!/usr/bin/python
-import pymysql
 import init_db
 import json2db
+import os
 
 
 __author__ = 'SimonSK'
 
 
+def add_tag(connection, tag, type):
+    with connection.cursor() as cursor:
+        # Insert a row for every packet. ignore if the primary key already exists.
+        sql = "INSERT IGNORE INTO Tag (tag, src)" \
+              "VALUES (%s, %s)"
+        cursor.execute(sql, (tag, type))
+    connection.commit()
+    print('new tag added!')
+
+
+def add_tagged(connection, tagid, pcapid, pin):
+    with connection.cursor() as cursor:
+        # Insert a row for every packet. ignore if the primary key already exists.
+        sql = "INSERT IGNORE INTO Tagged (tagid, pcapid, pin)" \
+              "VALUES (%s, %s, %s)"
+        cursor.execute(sql, (tagid, pcapid, pin))
+    connection.commit()
+    print('packet tagged!')
+
+
 def main():
     print("testing with dummy data (dhcp.pcap, pcap2.json")
-    msg = "1. tables reset and packets from dhcp.pcap added to the packet table\n" \
-          "2. dummy tag (Bob) created on pin=2 packet's SRC (192.168.0.1)\n" \
-          "3. add packets from pcap2.json (which is just a slightly modified version of dhcp.pcap)\n" \
+    print("1. tables reset and packets from dhcp.pcap added to the packet table")
+    init_db.main()
+    os.system("./pcap2db.sh ../pcaps/dhcp.pcap")
+    print("2. dummy tag (Bob) created on pin=2 packet's SRC (192.168.0.1)")
+    connection = init_db.connect_database()
+    add_tag(connection, 'Bob', 'SRC')
+    add_tagged(connection, 1, 'dhcp.pcap', 2)
+    connection.close()
+    msg = "3. add packets from pcap2.json (which is just a slightly modified version of dhcp.pcap)" \
           "all packets 192.168.0.1 are automatically tagged with Bob" \
           "Tagged table should have 4 entries."
-    init_db.main()
-    json2db.main(["../pcaps/dhcp.pcap"])
-    print("done")
+    print(msg)
+    json2db.main(['./pcap2.json'])
+    print("finished adding dummy data")
 
 
 if __name__ == "__main__":
