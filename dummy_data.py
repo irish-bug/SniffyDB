83b8bf52c5bb2342cf2368941d07fe850a48cade
SimonSK <simonsk@outlook.com>
2015-11-24 21:12:55 -0600

    script for adding dummy data

--- dummy_data.py
+++ dummy_data.py
@@ -29,20 +29,23 @@ def add_tagged(connection, tagid, pcapid, pin):
 
 def main():
     print("testing with dummy data (dhcp.pcap, pcap2.json")
-    print("1. tables reset and packets from dhcp.pcap added to the packet table")
-    init_db.main()
     os.system("./pcap2db.sh ../pcaps/dhcp.pcap")
-    print("2. dummy tag (Bob) created on pin=2 packet's SRC (192.168.0.1)")
+
     connection = init_db.connect_database()
     add_tag(connection, 'Bob', 'SRC')
     add_tagged(connection, 1, 'dhcp.pcap', 2)
     connection.close()
-    msg = "3. add packets from pcap2.json (which is just a slightly modified version of dhcp.pcap)" \
-          "all packets 192.168.0.1 are automatically tagged with Bob" \
-          "Tagged table should have 4 entries."
-    print(msg)
+
+    init_db.main()
     json2db.main(['./pcap2.json'])
     print("finished adding dummy data")
+
+    msg = "1. tables reset and packets from dhcp.pcap added to the packet table\n" \
+          "2. dummy tag (Bob) created on pin=2 packet's SRC (192.168.0.1)\n" \
+          "3. add packets from pcap2.json (which is just a slightly modified version of dhcp.pcap)\n" \
+          "all packets 192.168.0.1 are automatically tagged with Bob\n" \
+          "Tagged table should have 4 entries."
+    print(msg)
 
 
 if __name__ == "__main__":
