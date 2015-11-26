67db95b57b8c10b0ea72ff60572576b3bfedb0fc
SimonSK <simonsk@outlook.com>
2015-11-26 10:54:10 -0600

    debugging

--- init_db.py
+++ init_db.py
@@ -81,7 +81,7 @@ def create_tag(connection):
               "tag VARCHAR(255) NOT NULL," \
               "type VARCHAR(3) NOT NULL," \
               "PRIMARY KEY (tagid)" \
-              "UNIQUE (tag, type)" \
+              "UNIQUE KEY (tag, type)" \
               ")"
         cursor.execute(sql)
         connection.commit()
