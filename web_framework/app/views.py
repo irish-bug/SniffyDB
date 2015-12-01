42f8570f622785d0c129d89f0227ae27a36beb43
SimonSK <simonsk@outlook.com>
2015-12-01 02:58:22 -0600

    two tag type tuples

--- web_framework/app/views.py
+++ web_framework/app/views.py
@@ -187,13 +187,30 @@ def view_page():
 			pcapid=row['pcapid'],
 			packettime=row['packettime'],
 			PIN=row['pin'])
-		tag_query = """SELECT tag FROM Packet P, Tag T, Tagged Tg WHERE P.pcapid=Tg.pcapid AND P.pin=Tg.pin AND T.tagid=Tg.tagid AND P.pcapid=%s AND P.pin=%s""" % ("'"+row['pcapid']+"'", row['pin'])		
+#		tag_query = """SELECT tag FROM Packet P, Tag T, Tagged Tg WHERE P.pcapid=Tg.pcapid AND P.pin=Tg.pin AND T.tagid=Tg.tagid AND P.pcapid=%s AND P.pin=%s""" % ("'"+row['pcapid']+"'", row['pin'])
+#		new_cur = db.query(tag_query)
+#		if len(new_cur) == 0:
+#			temp['tag'] = ''
+#		else:
+#			temp['tag'] = new_cur[0]['tag']
+#		entries.append(temp)
+		tag_query = """SELECT tag, type FROM Packet P, Tag T, Tagged Tg WHERE P.pcapid=Tg.pcapid AND P.pin=Tg.pin AND T.tagid=Tg.tagid AND P.pcapid=%s AND P.pin=%s""" % ("'"+row['pcapid']+"'", row['pin'])
 		new_cur = db.query(tag_query)
 		if len(new_cur) == 0:
 			temp['tag'] = ''
+			temp['type'] = ''
+			entries.append(temp)
+		elif len(new_cur) == 1:
+			temp['tag'] = new_cur[0]['tag']
+			temp['type'] = new_cur[0]['type']
+			entries.append(temp)
 		else:
 			temp['tag'] = new_cur[0]['tag']
-		entries.append(temp)
+			temp['type'] = new_cur[0]['type']
+			entries.append(temp)
+			temp['tag'] = new_cur[0]['tag']
+			temp['type'] = new_cur[0]['type']
+			entries.append(temp)
 	return render_template('view_page.html', entries=entries)
 
 # For a given file, return whether it's an allowed type or not
