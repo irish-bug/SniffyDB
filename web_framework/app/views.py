131ce616451c1a4bc256f0ba6276aededa266b96
SimonSK <simonsk@outlook.com>
2015-12-01 12:43:32 -0600

    fixing add/edit page

--- web_framework/app/views.py
+++ web_framework/app/views.py
@@ -190,8 +190,9 @@ def view_page():
 					protocol=row['protocol'],
 					pcapid=row['pcapid'],
 					packettime=row['packettime'],
-					PIN=row['pin'],
-					tagged='No')
+					pin=row['pin'],
+					src_tag="",
+					dst_tag="")
 		#		tag_query = """SELECT tag FROM Packet P, Tag T, Tagged Tg WHERE P.pcapid=Tg.pcapid AND P.pin=Tg.pin AND T.tagid=Tg.tagid AND P.pcapid=%s AND P.pin=%s""" % ("'"+row['pcapid']+"'", row['pin'])
 		#		new_cur = db.query(tag_query)
 		#		if len(new_cur) == 0:
@@ -201,20 +202,18 @@ def view_page():
 		#		entries.append(temp)
 		tag_query = """SELECT tag, type FROM Packet P, Tag T, Tagged Tg WHERE P.pcapid=Tg.pcapid AND P.pin=Tg.pin AND T.tagid=Tg.tagid AND P.pcapid=%s AND P.pin=%s""" % ("'"+row['pcapid']+"'", row['pin'])
 		new_cur = db.query(tag_query)
-		if len(new_cur) != 0:
-			temp['tagged'] = 'Yes'
-			if len(new_cur) == 1:
-				if new_cur[0]['type'] == 'SRC':
-					temp['src'] += " (%s)" % new_cur[0]['tag']
-				else:
-					temp['dst'] += " (%s)" % new_cur[0]['tag']
+		if len(new_cur) == 1:
+			if new_cur[0]['type'] == 'SRC':
+				temp['src_tag'] = new_cur[0]['tag']
 			else:
-				if new_cur[0]['type'] == 'SRC':
-					temp['src'] += " (%s)" % new_cur[0]['tag']
-					temp['dst'] += " (%s)" % new_cur[1]['tag']
-				else:
-					temp['src'] += " (%s)" % new_cur[1]['tag']
-					temp['dst'] += " (%s)" % new_cur[0]['tag']
+				temp['dst_tag'] = new_cur[0]['tag']
+		elif len(new_cur) == 2:
+			if new_cur[0]['type'] == 'SRC':
+				temp['src_tag'] = new_cur[0]['tag']
+				temp['dst_tag'] = new_cur[1]['tag']
+			else:
+				temp['src_tag'] = new_cur[1]['tag']
+				temp['dst_tag'] = new_cur[0]['tag']
 		entries.append(temp)
 	return render_template('view_page.html', entries=entries)
 
@@ -231,21 +230,23 @@ def db_get_request(pcapid, pin):
 					payload=row['payload'] if row['payload'] == 'None' else 'Cannot be displayed',
 					pcapid=row['pcapid'],
 					packettime=row['packettime'],
-					pin=row['pin'])
+					pin=row['pin'],
+					src_tag="",
+					dst_tag="")
 		tag_query = """SELECT tag, type FROM Packet P, Tag T, Tagged Tg WHERE P.pcapid=Tg.pcapid AND P.pin=Tg.pin AND T.tagid=Tg.tagid AND P.pcapid=%s AND P.pin=%s""" % ("'"+row['pcapid']+"'", row['pin'])
 		new_cur = db.query(tag_query)
 		if len(new_cur) == 1:
 			if new_cur[0]['type'] == 'SRC':
-				temp['src'] += " (%s)" % new_cur[0]['tag']
+				temp['src_tag'] = new_cur[0]['tag']
 			else:
-				temp['dst'] += " (%s)" % new_cur[0]['tag']
+				temp['dst_tag'] = new_cur[0]['tag']
 		elif len(new_cur) == 2:
 			if new_cur[0]['type'] == 'SRC':
-				temp['src'] += " (%s)" % new_cur[0]['tag']
-				temp['dst'] += " (%s)" % new_cur[1]['tag']
+				temp['src_tag'] = new_cur[0]['tag']
+				temp['dst_tag'] = new_cur[1]['tag']
 			else:
-				temp['src'] += " (%s)" % new_cur[1]['tag']
-				temp['dst'] += " (%s)" % new_cur[0]['tag']
+				temp['src_tag'] = new_cur[1]['tag']
+				temp['dst_tag'] = new_cur[0]['tag']
 		entries.append(temp)
 	return entries
 
